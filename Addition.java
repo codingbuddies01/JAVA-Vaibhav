7d77def1316aed2f2e21b14ee305be20ab202729
Vaibhavchavan11 <chavanvaibhav775@gmail.com>
2024-09-04 14:20:58 +0530

    Addition.java

--- Addition.java
+++ Addition.java
@@ -1,7 +1,18 @@
-Public class Addition {
-  public static void main(String[] args) {
-    int x = 5;
-    int y = 3;
-    System.out.println(x + y);
-  }
+import java.util.Scanner;
+
+public class Addition {
+
+    public static int calculateSum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int sum = calculateSum(a, b);
+        System.out.print(sum);
+    }
 }
